649be440c4127782e46b069e537de6a3f74c5dbe
Florian GOTO <you@example.com>
2021-02-13 13:14:08 +0100

    dealing with arithmetic operators

---
+++ 1.10/exercise/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	count := 5
+
+	count += 5
+
+	fmt.Println(count)
+
+	count++
+
+	fmt.Println(count)
+
+	count++
+
+	fmt.Println(count)
+
+	count--
+
+	fmt.Println(count)
+
+	count -= 5
+
+	fmt.Println(count)
+
+}
