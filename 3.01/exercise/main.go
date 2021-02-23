5197675327960a90a2f052d7348bd26aad4264ea
Florian GOTO <you@example.com>
2021-02-23 17:18:13 +0100

    deal with booleans implementing password cheker

--- 3.01/exercise/main.go
+++ 3.01/exercise/main.go
@@ -1,14 +1,53 @@
 package main
 
-import(
-
+import (
 	"fmt"
-
+	"unicode"
 )
+
+func passwordCker(pw string) bool {
+	// convert pw into slice of Rune, safe for utf8
+	pwR := []rune(pw)
+
+	if len(pwR) < 8 {
+		return false
+	}
+
+	hasUpper := false
+	hasLower := false
+	hasNumber := false
+	hasSymbol := false
+
+	for _, v := range pwR {
+		if unicode.IsUpper(v) {
+			hasUpper = true
+		}
+
+		if unicode.IsLower(v) {
+			hasLower = true
+		}
+
+		if unicode.IsNumber(v) {
+			hasNumber = true
+		}
+
+		if unicode.IsPunct(v) || unicode.IsSymbol(v) {
+			hasSymbol = true
+		}
+
+	}
+
+	return hasUpper && hasLower && hasNumber && hasSymbol
+}
 
 func main() {
 
-	fmt.Println("init")
+	uPw := "Not-saFe"
+
+	sPw := "5*o/Ti#g56i9l8"
+
+	fmt.Printf("\"%s\" safe password : %v\n", uPw, passwordCker(uPw))
+
+	fmt.Printf("\"%s\" safe password : %v\n", sPw, passwordCker(sPw))
 
 }
-
