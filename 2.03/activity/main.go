a0fe64e140558046588219367585b7ae38690d7b
Florian GOTO <you@example.com>
2021-02-23 16:30:13 +0100

    implement bubble sort with in place swap

--- 2.03/activity/main.go
+++ 2.03/activity/main.go
@@ -1,14 +1,24 @@
 package main
 
-import(
-
+import (
 	"fmt"
-
 )
 
 func main() {
+	numbers := []int{4, 2, 9, 6, 1, 3, 5}
 
-	fmt.Println("init")
+	fmt.Println(numbers)
+
+	for i := 0; i < len(numbers); i++ {
+		for j := 0; j < len(numbers); j++ {
+			if numbers[i] < numbers[j] {
+				// in plaace swap
+				numbers[i], numbers[j] = numbers[j], numbers[i]
+			}
+
+		}
+	}
+
+	fmt.Println(numbers)
 
 }
-
