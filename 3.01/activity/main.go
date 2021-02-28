94e66435b42f44d365faa9b5935403fb9e83169c
Florian GOTO <you@example.com>
2021-02-28 12:23:53 +0100

    create sales tax calculator

--- 3.01/activity/main.go
+++ 3.01/activity/main.go
@@ -1,14 +1,38 @@
 package main
 
-import(
-
+import (
 	"fmt"
-
 )
+
+type Item struct {
+	name    string
+	cost    float32
+	taxRate float32
+}
 
 func main() {
 
-	fmt.Println("init")
+	/*var message *string
 
+	if message == nil {
+		fmt.Println("error, unexpected nil value")
+		return
+	}
+
+	fmt.Println(&message)
+	*/
+
+	items := []Item{
+		Item{"cake", 0.99, 0.075},
+		Item{"milk", 2.75, 0.015},
+		Item{"butter", 0.87, 0.02},
+	}
+
+	var salesTaxTotal float32
+
+	for _, item := range items {
+		salesTaxTotal += item.cost * item.taxRate
+	}
+
+	fmt.Println("Sales Tax Total:", salesTaxTotal)
 }
-
