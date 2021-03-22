89f0c616076cf126295eff4071a56b40b77cd2b7
Florian GOTO <you@example.com>
2021-03-22 20:06:17 +0100

    loop over an array with classic for loop

--- 4.06/exercise/main.go
+++ 4.06/exercise/main.go
@@ -1,14 +1,23 @@
 package main
 
-import(
+import "fmt"
 
-	"fmt"
+func buildMessage() string {
+	m := ""
+	arr := [4]int{11, 22, 33, 44}
 
-)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = arr[i] * arr[i]
+		m += fmt.Sprintf("%v: %v\n", i, arr[i])
+	}
 
-func main() {
-
-	fmt.Println("init")
-
+	return m
 }
 
+func main() {
+	// creating array from condtant integer variable
+	const lastIndex = 4
+	arr := [lastIndex]string{}
+	fmt.Println(arr)
+	fmt.Println(buildMessage())
+}
