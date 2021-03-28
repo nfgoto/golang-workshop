9690008ba7edb8516b3920012e6d01829bc64f4f
Florian GOTO <you@example.com>
2021-03-28 18:47:11 +0200

    Implement longuest command argument finder with slice

--- 4.08/exercise/main.go
+++ 4.08/exercise/main.go
@@ -1,14 +1,39 @@
 package main
 
-import(
-
+import (
 	"fmt"
-
+	"os"
 )
 
-func main() {
+func getPassedArgs(minArgs int) []string {
+	if len(os.Args) < minArgs {
+		fmt.Printf("At least %v arguments are needed]n", minArgs)
+		os.Exit(1)
+	}
 
-	fmt.Println("init")
-
+	var args []string
+	for i := 1; i < len(os.Args); i++ {
+		args = append(args, os.Args[i])
+	}
+	return args
 }
 
+func findLonguestWord(args []string) string {
+	var longuest string
+	for i := 0; i < len(args); i++ {
+		if len(args[i]) > len(longuest) {
+			longuest = args[i]
+		}
+	}
+	return longuest
+}
+
+func main() {
+	fmt.Printf("os.Args:\n%v\n\n", os.Args)
+	if longuest := findLonguestWord(getPassedArgs(3)); len(longuest) > 0 {
+		fmt.Println("Longuest command argument was:", longuest)
+	} else {
+		fmt.Println("Error occurred")
+		os.Exit(1)
+	}
+}
